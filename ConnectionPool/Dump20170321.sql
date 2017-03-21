ef8982f4269ad41c736077ec54502140cc424022
cong.pc <congpc@tctav.com>
2017-03-21 14:13:14 +0700

    Add files

---
+++ ConnectionPool/Dump20170321.sql
@@ -0,0 +1,84 @@
+-- MySQL dump 10.13  Distrib 5.7.9, for osx10.9 (x86_64)
+--
+-- Host: localhost    Database: exampleDb
+-- ------------------------------------------------------
+-- Server version	5.7.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `lines`
+--
+
+DROP TABLE IF EXISTS `lines`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lines` (
+  `lineID` int(11) NOT NULL AUTO_INCREMENT,
+  `lineName` varchar(45) DEFAULT 'Name',
+  `productName` varchar(45) DEFAULT 'Product',
+  `orderID` int(11) DEFAULT '0',
+  PRIMARY KEY (`lineID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lines`
+--
+
+LOCK TABLES `lines` WRITE;
+/*!40000 ALTER TABLE `lines` DISABLE KEYS */;
+/*!40000 ALTER TABLE `lines` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `orders` (
+  `orderID` int(11) NOT NULL AUTO_INCREMENT,
+  `customerID` int(11) DEFAULT '0',
+  PRIMARY KEY (`orderID`),
+  UNIQUE KEY `orderID_UNIQUE` (`orderID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'exampleDb'
+--
+
+--
+-- Dumping routines for database 'exampleDb'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-03-21 11:55:45
---
+++ ConnectionPool/src/com/congpc/servlet/ConnectionPoolServlet.java
@@ -0,0 +1,223 @@
+package com.congpc.servlet;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+//import java.io.*;
+import java.sql.*;
+import javax.naming.*;
+import javax.sql.*;
+
+/**
+ * Servlet implementation class ConnectionPoolServlet
+ */
+@WebServlet("/ConnectionPool")
+public class ConnectionPoolServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static long pooledDuration, nonPooledDuration;
+	private static long pooledCount, nonPooledCount;
+	private DataSource datasource = null;
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ConnectionPoolServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    public void init(ServletConfig config) throws ServletException {
+        try {
+          //Create a datasource for pooled connections.
+          //Register the driver for non pooled connections.
+          Context initCtx = new InitialContext();
+          Context envCtx = (Context) initCtx.lookup("java:comp/env");
+          datasource = (DataSource) envCtx.lookup("jdbc/ConnectionPool");
+          Class.forName("com.mysql.jdbc.Driver").newInstance();
+        }
+        catch (Exception e) {
+          //throw new ServletException(e.getMessage());
+          e.printStackTrace();
+        }
+      }
+
+      private Connection getConnection(boolean pooledConnection) throws SQLException {
+        if (pooledConnection) {
+          pooledCount++;
+          return datasource.getConnection();
+        }
+        else {
+          nonPooledCount++;
+          Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/exampleDb", "congpc", "Demo_1987");
+          return con;
+        }
+      }
+      private Connection getPostConnection(boolean pooledConnection) throws SQLException {
+          if (pooledConnection) {
+            return datasource.getConnection();
+          }
+          else {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/exampleDb", "congpc", "Demo_1987");
+            return con;
+          }
+        }
+      
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		String queryStr = req.getQueryString();
+	    boolean poolingEnabled = queryStr == null || !queryStr.equals("disablePooling");
+		//boolean poolingEnabled = false;
+	    PrintWriter	out = res.getWriter();
+	    res.setContentType("text/html");
+	    out.println("<html><head><title>Orders</title></head><body>");
+	    out.println("<br>PooledConnectionCount:"+pooledCount+", nonPooledConnectionCount:"+nonPooledCount+"<br>");
+	    if (pooledDuration > 0) {
+	      out.println("<br>"+ "Average pooled response:"+pooledDuration/pooledCount);
+	    }
+	    if (nonPooledDuration > 0) {
+	    		out.println("<br>"+ "Average non pooled response:"+nonPooledDuration/nonPooledCount);
+	    }
+
+	    Connection connection = null;
+	    Statement stmt = null;
+	    ResultSet rs = null;
+	    
+	    long startTime = System.currentTimeMillis();
+
+	    try {
+	      connection = getConnection(poolingEnabled);
+	      stmt = connection.createStatement();
+	      String sqlSelect = "SELECT exampleDb.orders.orderID, exampleDb.orders.customerID, exampleDb.lines.lineID, exampleDb.lines.lineName, exampleDb.lines.productName FROM exampleDb.orders INNER JOIN exampleDb.lines ON exampleDb.lines.orderID = exampleDb.orders.orderID ORDER BY exampleDb.orders.orderID";
+	      rs = stmt.executeQuery(sqlSelect);
+	      ResultSetMetaData dbMeta = rs.getMetaData();
+	      out.println("<br><table border='1'>");
+
+	      //Create the table headers
+
+	      out.println("<tr>");
+	      for (int col=0; col<dbMeta.getColumnCount(); col++) {
+	        out.println("<th>" + dbMeta.getColumnName(col+1) + "</th>");
+	      }
+	      out.println("</tr>");
+
+	      //Create the table data
+
+	      while (rs.next()) {
+	        out.println("<tr>");
+	        for (int col=0; col < dbMeta.getColumnCount(); col++) {
+	          out.println("<td>" + rs.getString(col+1) + "</td>");
+	        }
+	        out.println("</tr>");
+	      }
+	      out.println("</table>");
+	      out.println("</body></html>");
+	      connection.close();
+	    }
+	    catch (SQLException e) {
+	      //throw new ServletException(e.getMessage());
+	      e.printStackTrace();
+	    }
+	    finally {
+	      try {if (rs != null) rs.close();} catch (SQLException e) {e.printStackTrace();}
+	      try {if (stmt != null) stmt.close();} catch (SQLException e) {e.printStackTrace();}
+	      try {if (connection != null) connection.close();} catch (SQLException e) {e.printStackTrace();}
+	      long elapsed = System.currentTimeMillis() - startTime;
+	      //Collect the times
+	      if (poolingEnabled)
+	        this.pooledDuration += elapsed;
+	      else
+	    		this.nonPooledDuration += elapsed;
+	    }
+	}
+	
+//	private void executeSqlCommands(boolean batchEnabled, boolean prepareEnabled, int loopCount) throws SQLException {
+//		
+//	}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+//		String queryStr = req.getQueryString();
+//		boolean poolingEnabled = queryStr == null || !queryStr.equals("disablePooling");
+//		boolean batchEnabled = queryStr == null || !queryStr.equals("disableBatch");
+//		boolean prepareEnabled = queryStr == null || !queryStr.equals("disablePrepare");
+		boolean poolingEnabled = true;
+		boolean batchEnabled = false;
+		boolean prepareEnabled = true;
+		int loopCount = 1000;
+		Connection connection = null;
+		PreparedStatement stmtp1 = null;
+		Statement stmt2 = null;
+		PreparedStatement stmtp2 = null;
+		boolean resultFlag = false;
+		String errorMessage = "";
+		long startTime = System.currentTimeMillis();
+		try {
+			for (int i = 1; i <= loopCount; i++) {
+				connection = getPostConnection(poolingEnabled);
+				if (batchEnabled) {
+					
+				} else {
+					long orderID = 0;
+					String sqlOrderInsert = "insert into exampleDb.orders (customerID) values (?)";
+					stmtp1 = connection.prepareStatement(sqlOrderInsert, Statement.RETURN_GENERATED_KEYS);
+					stmtp1.setLong(1, i);
+					int affectedRows = stmtp1.executeUpdate();
+					if (affectedRows == 0) {
+						throw new SQLException("Creating order failed, no rows affected.");
+					}
+					try (ResultSet generatedKeys = stmtp1.getGeneratedKeys()) {
+						if (generatedKeys.next()) {
+							orderID = generatedKeys.getLong(1);
+						}else {
+							throw new SQLException("Creating user failed, no ID obtained.");
+						}
+					}
+					String name = "name-"+ orderID;
+					String product = "product-"+ orderID;
+					if (prepareEnabled) {
+						String sqlLineInsert = "insert into exampleDb.lines (lineName,productName,orderID) values (?,?,?)";
+						stmtp2 = connection.prepareStatement(sqlLineInsert);
+						stmtp2.setString(1, name);
+						stmtp2.setString(2, product);
+						stmtp2.setLong(3, orderID);
+						stmtp2.executeUpdate();
+					} else {
+						String sqlLineInsert = "insert into exampleDb.lines (lineName,productName,orderID) values ('"+name+"','" + product + "','" + orderID +"')";
+						stmt2 = connection.createStatement();
+						stmt2.executeUpdate(sqlLineInsert);
+					}
+				}
+				resultFlag = true;
+				connection.close();
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+			errorMessage = e.getMessage();
+		}
+		finally {
+			try {if (stmtp1 != null) stmtp1.close();} catch (SQLException e) {e.printStackTrace();}
+			try {if (stmt2 != null) stmt2.close();} catch (SQLException e) {e.printStackTrace();}
+			try {if (stmtp2 != null) stmtp2.close();} catch (SQLException e) {e.printStackTrace();}
+			try {if (connection != null) connection.close();} catch (SQLException e) {e.printStackTrace();}
+			long elapsed = System.currentTimeMillis() - startTime;
+			
+			res.setContentType("text/html");
+			res.setCharacterEncoding("UTF-8");
+			PrintWriter out = res.getWriter();
+			if (resultFlag) {
+				out.append("{code:200,elapsed:"+elapsed+"}");
+			} else {
+				out.append("{code:300,errorMessage:"+ errorMessage + "}");
+			}
+			out.close();
+		}
+	}
+
+}
